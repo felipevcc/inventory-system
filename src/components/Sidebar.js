9a42138d93ee12c249db787f6b3e4a28a5b6b1ea
felipevcc <felipevillamizarc@gmail.com>
2023-05-31 22:34:44 -0500

    Sidebar button fixed when a view is open

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -21,6 +21,12 @@ const Sidebar = () => {
         navigate('/login');
     };
 
+    // Selected view
+    const [selectedView, setSelectedView] = useState("");
+    const handleSelectedView = (view) => {
+        setSelectedView(view);
+    };
+
     return (
         <nav className={`sidebar-container ${isSidebarOpen ? 'close' : ''}`}>
             <header>
@@ -42,44 +48,44 @@ const Sidebar = () => {
             <div className="menu-bar">
                 <div className="menu">
                     <ul className="menu-links">
-                        <li className="nav-link">
-                            <Link to="/home">
+                        <li className={`nav-link ${selectedView === "home" ? "selected" : ""}`}>
+                            <Link to="/home" onClick={() => handleSelectedView("home")}>
                                 <FontAwesomeIcon icon={faHouse} className="icon" />
                                 <span className="text nav-text">Inicio</span>
                             </Link>
                         </li>
-                        <li className="nav-link">
-                            <Link to="/categories">
+                        <li className={`nav-link ${selectedView === "categories" ? "selected" : ""}`}>
+                            <Link to="/categories" onClick={() => handleSelectedView("categories")}>
                                 <FontAwesomeIcon icon={faTags} className="icon" />
                                 <span className="text nav-text">Categorías</span>
                             </Link>
                         </li>
-                        <li className="nav-link">
-                            <Link to="/items">
+                        <li className={`nav-link ${selectedView === "items" ? "selected" : ""}`}>
+                            <Link to="/items" onClick={() => handleSelectedView("items")}>
                                 <FontAwesomeIcon icon={faBoxesStacked} className="icon" />
                                 <span className="text nav-text">Artículos</span>
                             </Link>
                         </li>
-                        <li className="nav-link">
-                            <Link to="/providers">
+                        <li className={`nav-link ${selectedView === "providers" ? "selected" : ""}`}>
+                            <Link to="/providers" onClick={() => handleSelectedView("providers")}>
                                 <FontAwesomeIcon icon={faTruckFast} className="icon" />
                                 <span className="text nav-text">Proveedores</span>
                             </Link>
                         </li>
-                        <li className="nav-link">
-                            <Link to="/purchases">
+                        <li className={`nav-link ${selectedView === "purchases" ? "selected" : ""}`}>
+                            <Link to="/purchases" onClick={() => handleSelectedView("purchases")}>
                                 <FontAwesomeIcon icon={faBasketShopping} className="icon" />
                                 <span className="text nav-text">Compras</span>
                             </Link>
                         </li>
-                        <li className="nav-link">
-                            <Link to="/customers">
+                        <li className={`nav-link ${selectedView === "customers" ? "selected" : ""}`}>
+                            <Link to="/customers" onClick={() => handleSelectedView("customers")}>
                                 <FontAwesomeIcon icon={faUsers} className="icon" />
                                 <span className="text nav-text">Clientes</span>
                             </Link>
                         </li>
-                        <li className="nav-link">
-                            <Link to="/sales">
+                        <li className={`nav-link ${selectedView === "sales" ? "selected" : ""}`}>
+                            <Link to="/sales" onClick={() => handleSelectedView("sales")}>
                                 <FontAwesomeIcon icon={faHandHoldingDollar} className="icon" />
                                 <span className="text nav-text">Ventas</span>
                             </Link>
@@ -87,8 +93,8 @@ const Sidebar = () => {
                     </ul>
                 </div>
                 <div className="bottom-content">
-                    <li className="">
-                        <Link to="/users">
+                    <li className={`${selectedView === "users" ? "selected" : ""}`}>
+                        <Link to="/users" onClick={() => handleSelectedView("users")}>
                             <FontAwesomeIcon icon={faUsersGear} className="icon" />
                             <span className="text nav-text">Usuarios</span>
                         </Link>
